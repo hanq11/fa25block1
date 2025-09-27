7b8c19259e979848ac2b02477fbc4d59178101de
hanq11 <>
2025-09-27 20:05:00 +0700

    update

---
+++ sd20204/src/main/java/com/example/sd20204/buoi5/service/SinhVienService.java
@@ -0,0 +1,48 @@
+package com.example.sd20204.buoi5.service;
+
+import com.example.sd20204.buoi5.model.SinhVien;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class SinhVienService {
+    private List<SinhVien> danhSach = null;
+    public SinhVienService() {
+        danhSach = Arrays.asList(
+          new SinhVien(1, "Nguyen Van A", 5.6f, true),
+          new SinhVien(2, "Nguyen Van B", 6.6f, true),
+          new SinhVien(3, "Nguyen Thi C", 8.6f, false)
+        );
+    }
+    public List<SinhVien> getAll() {
+        return danhSach;
+    }
+
+    public SinhVien getDetail(Integer id) {
+        for(SinhVien sv: danhSach) {
+            if(sv.getId() == id) {
+                return sv;
+            }
+        }
+        return null;
+    }
+
+    public void addSinhVien(SinhVien sinhVien) {
+        danhSach.add(sinhVien);
+    }
+    public void updateSinhVien(SinhVien sinhVien) {
+        int updateIndex = -1;
+        for(int i = 0; i < danhSach.size(); i++) {
+            if(danhSach.get(i).getId() == sinhVien.getId()) {
+                updateIndex = i;
+            }
+        }
+        danhSach.set(updateIndex, sinhVien);
+    }
+    public void deleteSinhVien(Integer id) {
+        danhSach.removeIf(sv -> sv.getId() == id);
+    }
+
+}
