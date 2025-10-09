19fc9a7512acf2e19cec558af05f5fec72553f9c
hanq11 <hanq11@fpt.edu.vn>
2025-10-09 13:23:18 +0700

    update

--- tutorsof3022/src/main/java/com/example/tutorsof3022/controller/NhanVienController.java
+++ tutorsof3022/src/main/java/com/example/tutorsof3022/controller/NhanVienController.java
@@ -1,11 +1,11 @@
 package com.example.tutorsof3022.controller;
 
+import com.example.tutorsof3022.model.NhanVien;
 import com.example.tutorsof3022.service.NhanVienService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/nhan-vien")
@@ -17,5 +17,29 @@ public class NhanVienController {
     public String hienThi(Model model) {
         model.addAttribute("danhSach", nhanVienService.getAllNhanVien());
         return "/nhanVien/hienThi"; // thymeleaf
+    }
+
+    @PostMapping("/them")
+    public String themNhanVien(NhanVien nhanVien) {
+        nhanVienService.addNhanVien(nhanVien);
+        return "redirect:/nhan-vien/hien-thi";
+    }
+
+    @GetMapping("/update/{id}")
+    public String viewUpdateNhanVien(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("nhanVien", nhanVienService.getNhanVienById(id));
+        return "/nhanVien/viewUpdate";
+    }
+
+    @PostMapping("/update")
+    public String updateNhanVien(NhanVien nhanVien) {
+        nhanVienService.updateNhanVien(nhanVien);
+        return "redirect:/nhan-vien/hien-thi";
+    }
+
+    @GetMapping("/delete")
+    public String deleteNhanVien(@RequestParam("id") Integer id) {
+        nhanVienService.removeNhanVienById(id);
+        return "redirect:/nhan-vien/hien-thi";
     }
 }
