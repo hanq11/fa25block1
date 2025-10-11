56e313ed35582f2f1dc041ca9d69a4f6e76f3061
hanq11 <>
2025-10-11 18:04:08 +0700

    update

--- sd20203/src/main/java/com/example/sd20203/buoi10/controller/SanPhamController.java
+++ sd20203/src/main/java/com/example/sd20203/buoi10/controller/SanPhamController.java
@@ -5,12 +5,12 @@ import com.example.sd20203.buoi10.model.SanPham;
 import com.example.sd20203.buoi10.repository.SanPhamRepository;
 import com.example.sd20203.buoi10.service.DanhMucService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,6 +32,10 @@ public class SanPhamController {
     public String hienThi(Model model) {
         model.addAttribute("listSanPham", sanPhamRepository.findAll());
 //        model.addAttribute("listDanhMuc", danhMucService.getAllDanhMuc());
+        System.out.println("Tim kiem JPQL:");
+        sanPhamRepository.timKiemSanPhamTheoKhoangGia(9F, 11F).forEach(sp -> System.out.println(sp.getTen()));
+        System.out.println("Tim kiem SQL:");
+        sanPhamRepository.timKiemSanPhamTheoKhoangGiaSQL(4F, 16F).forEach(sp -> System.out.println(sp.getTen()));
         return "/sanPham/hienThi";
     }
 
@@ -39,5 +43,39 @@ public class SanPhamController {
     public String themSanPham(SanPham sanPham) {
         sanPhamRepository.save(sanPham);
         return "redirect:/san-pham/hien-thi";
+    }
+
+    @GetMapping("/view-update/{id}")
+    public String viewUpdate(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("sanPham", sanPhamRepository.findById(id).get());
+        return "/sanPham/viewUpdate";
+    }
+
+    @PostMapping("/sua")
+    public String sua(SanPham sanPham) {
+        sanPhamRepository.save(sanPham);
+        return "redirect:/san-pham/hien-thi";
+    }
+
+    @GetMapping("/xoa")
+    public String xoaSanPham(@RequestParam("id") Integer id) {
+        sanPhamRepository.deleteById(id);
+        return "redirect:/san-pham/hien-thi";
+    }
+
+    @GetMapping("/phan-trang")
+    public String phanTrang(@RequestParam(name = "page", defaultValue = "0", required = false) Integer page, Model model) {
+        int pageSize = 2;
+        Sort sort = Sort.by(Sort.Direction.DESC, "ten");
+        Pageable pageable = PageRequest.of(page, pageSize, sort);
+        model.addAttribute("page", sanPhamRepository.findAll(pageable));
+        return "/sanPham/phanTrang";
+    }
+
+    @GetMapping("/tim-kiem")
+    public String timKiemTenGia(@RequestParam(name = "ten", required = false) String ten
+                                , Model model) {
+        model.addAttribute("listSanPham", sanPhamRepository.findSanPhamsByTenContains(ten));
+        return "/sanPham/hienThi";
     }
 }
--- sd20203/src/main/java/com/example/sd20203/buoi10/repository/SanPhamRepository.java
+++ sd20203/src/main/java/com/example/sd20203/buoi10/repository/SanPhamRepository.java
@@ -2,8 +2,24 @@ package com.example.sd20203.buoi10.repository;
 
 import com.example.sd20203.buoi10.model.SanPham;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SanPhamRepository extends JpaRepository<SanPham, Integer> {
+    // Query method
+    public List<SanPham> findSanPhamsByTenContains(String ten);
+
+    // JPQL -> huong toi class
+    @Query(value = "SELECT sp FROM SanPham sp WHERE sp.gia BETWEEN :min AND :max")
+    public List<SanPham> timKiemSanPhamTheoKhoangGia(@Param("min") Float min, @Param("max") Float max);
+
+
+    // SQL -> huong toi bang
+    @Query(value = "SELECT * FROM san_pham WHERE gia BETWEEN :min AND :max", nativeQuery = true)
+    public List<SanPham> timKiemSanPhamTheoKhoangGiaSQL(@Param("min") Float min, @Param("max") Float max);
+
 }
