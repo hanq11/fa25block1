d8fcd19dd455fdf70fbc9d550e5a3d497d0557df
hanq11 <hanq11@fpt.edu.vn>
2025-10-14 15:44:21 +0700

    update

--- tutorsof3022/src/main/java/com/example/tutorsof3022/controller/TaiSanController.java
+++ tutorsof3022/src/main/java/com/example/tutorsof3022/controller/TaiSanController.java
@@ -5,6 +5,10 @@ import com.example.tutorsof3022.model.TaiSan;
 import com.example.tutorsof3022.repository.NhanVienRepository;
 import com.example.tutorsof3022.repository.TaiSanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +32,10 @@ public class TaiSanController {
     public String hienThi(Model model) {
         model.addAttribute("listTaiSan"
                 , taiSanRepository.findAll());
+        System.out.println("JPQL");
+        taiSanRepository.searchTaiSanTheoTen("C").forEach(ts -> System.out.println(ts.getTen()));
+        System.out.println("SQL");
+        taiSanRepository.searchTaiSanTheoTenSQL("V").forEach(ts -> System.out.println(ts.getTen()));
         return "/taiSan/hienThi";
     }
 
@@ -54,5 +62,20 @@ public class TaiSanController {
     public String xoaTaiSan(@RequestParam("id") Integer id) {
         taiSanRepository.deleteById(id);
         return "redirect:/tai-san/hien-thi";
+    }
+
+    @GetMapping("/phan-trang")
+    public String phanTrang(@RequestParam(name = "page", defaultValue = "0", required = false) Integer page, Model model) {
+        int pageSize = 2;
+        Sort sort = Sort.by(Sort.Direction.DESC, "ten");
+        Pageable pageable = PageRequest.of(page, pageSize, sort);
+        model.addAttribute("page", taiSanRepository.findAll(pageable));
+        return "taiSan/phanTrang";
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam("ten") String ten, Model model) {
+        model.addAttribute("listTaiSan", taiSanRepository.findTaiSansByTenContains(ten));
+        return "/taiSan/hienThi";
     }
 }
--- tutorsof3022/src/main/java/com/example/tutorsof3022/repository/TaiSanRepository.java
+++ tutorsof3022/src/main/java/com/example/tutorsof3022/repository/TaiSanRepository.java
@@ -1,10 +1,22 @@
 package com.example.tutorsof3022.repository;
 
 import com.example.tutorsof3022.model.TaiSan;
-import org.hibernate.boot.archive.internal.JarProtocolArchiveDescriptor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TaiSanRepository extends JpaRepository<TaiSan, Integer> {
+    public List<TaiSan> findTaiSansByTenContains(String ten);
+
+    // JPQL
+    @Query("SELECT ts FROM TaiSan ts WHERE ts.ten LIKE %:ten%")
+    public List<TaiSan> searchTaiSanTheoTen(@Param("ten") String ten);
+
+    // SQL
+    @Query(value = "SELECT * FROM TaiSan WHERE ten LIKE %:ten%", nativeQuery = true)
+    public List<TaiSan> searchTaiSanTheoTenSQL(@Param("ten") String ten);
 }
